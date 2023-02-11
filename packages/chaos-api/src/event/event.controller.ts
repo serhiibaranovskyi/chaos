734d0a12f0763049a595fe0e586b07208b3ec8b3
Serhii Baranovskyi <baranovsky.sergii@gmail.com>
2023-02-11 12:59:23 +0200

    update event controller

--- packages/chaos-api/src/event/event.controller.ts
+++ packages/chaos-api/src/event/event.controller.ts
@@ -1,7 +1,40 @@
-import type { FastifyRequest } from 'fastify/types/request'
-import type { FastifyReply } from 'fastify/types/reply'
+import { FastifyRequest, FastifyReply, FastifyInstance } from 'fastify'
 
-import { prisma } from '../db'
+import {
+  EmptyResponseDto,
+  makeEmptyResponse,
+  makeResponse,
+} from '@/shared/response'
 
-import { IEventServiceInterface, IEventControllerInterface } from './interfaces'
-import { EventService } from './event.service'
+import type { CreateEventDto, EventIdDto, EventResponseDto } from './event.dto'
+
+export async function createEvent(
+  this: FastifyInstance,
+  request: FastifyRequest<{ Body: CreateEventDto }>
+): Promise<EventResponseDto> {
+  const event = await this.services.event.create(
+    request.body.topicId,
+    request.body.payload
+  )
+  return makeResponse(event)
+}
+
+export async function findEventById(
+  this: FastifyInstance,
+  request: FastifyRequest<{ Params: EventIdDto }>,
+  reply: FastifyReply
+): Promise<EventResponseDto> {
+  const event = await this.services.event.findById(request.params.id)
+  if (event === null) {
+    return reply.status(404).send(makeEmptyResponse({ ok: false }))
+  }
+  return makeResponse(event)
+}
+
+export async function deleteEvent(
+  this: FastifyInstance,
+  request: FastifyRequest<{ Params: EventIdDto }>
+): Promise<EmptyResponseDto> {
+  await this.services.event.delete(request.params.id)
+  return makeResponse(null)
+}
