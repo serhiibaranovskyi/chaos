b1c9ebfeb20c09226b6e3bcbcdaca2215660e02b
serhiibaranovskyi <baranovsky.sergii@gmail.com>
2023-02-18 12:47:14 +0200

    feat: let's forget about it

--- packages/chaos-web-client/.lintstagedrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // disable svelte for now https://github.com/sveltejs/prettier-plugin-svelte/issues/240
-  // '**/*.{svelte,ts,js,cjs}': 'eslint --cache --fix',
-  '**/*.{ts,js,cjs}': 'eslint --cache --fix',
-  './**/*.{json,md,html,css}': 'prettier --write',
-}
