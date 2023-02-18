17194fbd7bbed7a66506a371903f91c634ec67ee
Serhii Baranovskyi <baranovsky.sergii@gmail.com>
2023-02-18 14:25:09 +0200

    update topic route

--- packages/chaos-api/src/server.ts
+++ packages/chaos-api/src/server.ts
@@ -4,6 +4,10 @@ export const server: FastifyInstance = fastify({
   logger: true,
 })
 
+import { createTopic } from '@/topic/topic.route.v1'
+
+server.register(createTopic, { prefix: '/v1' })
+
 server.listen({ port: 8080 }, (err: Error | null) => {
   if (err) {
     server.log.error(err)
